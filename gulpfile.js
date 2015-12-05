0a368d09fd8a22ddfe4735e1abcb51da0efceaa1
Randy Burgess <wrburgess@gmail.com>
2015-12-05 13:22:44 -0600

    setup gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,40 @@
+'use strict';
+
+// Import modules
 var gulp = require('gulp');
 
+// Configuration
+var config = {
+  js: {
+    src: './src/js/main.jsx',
+    watch: './src/js/**/*',
+    outputDir: './build/'
+  },
+  css: {
+    src: './src/css/*.sass',
+    watch: './src/css/**/*',
+    outputDir: './build/'
+  },
+  html: {
+    src: './src/html/*',
+    watch: './src/html/**/*',
+    outputDir: './build/'
+  },
+  images: {
+    src: './src/images/*.sass',
+    watch: './src/images/**/*',
+    outputDir: './build/'
+  },
+  fonts: {
+    src: './src/fonts/*.sass',
+    watch: './src/fonts/**/*',
+    outputDir: './build/'
+  }
+};
+
+// Default task for Gulp
 gulp.task('default', function() {
-  // place code for your default task here
+  var bundler = browserify(config.js.src, args)
+
+  bundle(bundler);
 });
